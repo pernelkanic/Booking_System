6c968ea763bf16237b528b88f81bf455b76e3e43
pernelkanic <211001118@rajalakshmi.edu.in>
2024-07-30 21:56:58 +0530

    check commit

--- backend/BookingBackend/routes/booking-routes.go
+++ backend/BookingBackend/routes/booking-routes.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"booking-service/db"
 	"net/http"
 
-	contact "contact.com"
 	"github.com/go-chi/chi"
 )
 
-var mh *contact.MongoHandler
+var mh *db.Booking.MongoHandler
 
 func registerRoutes() http.Handler {
 	r := chi.NewRouter()
-	r.Route("/contacts", func(r chi.Router) {
+	r.Route("/apo", func(r chi.Router) {
 		r.Get("/", getAllbooking)             //GET /contacts
 		r.Get("/{bookingId}", getBookingbyId) //GET /contacts/0147344454
 		r.Post("/", postBooking)              //POST /contacts
